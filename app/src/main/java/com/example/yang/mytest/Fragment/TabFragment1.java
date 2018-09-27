c2ed83a3fba8317f3b1ebea7e9286b43d7f4a381
yangEdge <1191851376@qq.com>
2018-09-27 10:05:47 +0800

    1.1

--- app/src/main/java/com/example/yang/mytest/Fragment/TabFragment1.java
+++ app/src/main/java/com/example/yang/mytest/Fragment/TabFragment1.java
@@ -11,14 +11,16 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.yang.mytest.Adapter.UltraPagerAdapter;
+import com.example.yang.mytest.Bean.GetConfig;
 import com.example.yang.mytest.Fragment.HomePage.AirFragment;
 import com.example.yang.mytest.Fragment.HomePage.Co2Fragment;
 import com.example.yang.mytest.Fragment.HomePage.LightFragment;
 import com.example.yang.mytest.Fragment.HomePage.SoilFragment;
-import com.example.yang.mytest.Http.BaseHttp;
+import com.example.yang.mytest.Http.HttpUtils;
 import com.example.yang.mytest.Bean.ZhuYeReception;
 import com.example.yang.mytest.R;
 import com.tmall.ultraviewpager.UltraViewPager;
@@ -48,25 +50,55 @@ public class TabFragment1 extends Fragment {
     TextView tv_airHumidity;
 
     //设定值
-    @BindView(R.id.sheding_co2)
-    TextView sheding_co2;
-    @BindView(R.id.sheding_light)
-    TextView sheding_light;
-    @BindView(R.id.sheding_soilTemperature)
-    TextView sheding_soilTemperature;
-    @BindView(R.id.sheding_soilHumidity)
-    TextView sheding_soilHumidity;
-    @BindView(R.id.sheding_airTemperature)
-    TextView sheding_airTemperature;
-    @BindView(R.id.sheding_airHumidity)
-    TextView sheding_airHumidity;
+    @BindView(R.id.f1_minCo2)
+    TextView f1_minCo2;
+    @BindView(R.id.f1_maxCo2)
+    TextView f1_maxCo2;
+    @BindView(R.id.f1_minLight)
+    TextView f1_minLight;
+    @BindView(R.id.f1_maxLight)
+    TextView f1_maxLight;
+    @BindView(R.id.f1_minAirTemperature)
+    TextView f1_minAirTemperature;
+    @BindView(R.id.f1_maxAirTemperature)
+    TextView f1_maxAirTemperature;
+    @BindView(R.id.f1_minAirHumidity)
+    TextView f1_minAirHumidity;
+    @BindView(R.id.f1_maxAirHumidity)
+    TextView f1_maxAirHumidity;
+    @BindView(R.id.f1_minSoilTemperature)
+    TextView f1_minSoilTemperature;
+    @BindView(R.id.f1_maxSoilTemperature)
+    TextView f1_maxSoilTemperature;
+    @BindView(R.id.f1_minSoilHumidity)
+    TextView f1_minSoilHumidity;
+    @BindView(R.id.f1_maxSoilHumidity)
+    TextView f1_maxSoilHumidity;
+
+    //警告提示
+    @BindView(R.id.img_p_co2)
+    ImageView img_p_co2;
+    @BindView(R.id.img_p_light)
+    ImageView img_p_light;
+    @BindView(R.id.img_p_soil)
+    ImageView img_p_soil;
+    @BindView(R.id.img_p_air)
+    ImageView img_p_air;
 
     @BindView(R.id.myUlViewPager)
     UltraViewPager mUltraViewPager;
+
     private int[] imageArr = {R.drawable.bana1, R.drawable.bana2, R.drawable.bana3};
     private UltraPagerAdapter mAdapter;
     public boolean flag = true;
     private Thread thread;
+
+    private static int co2 = 0;
+    private static int light = 0;
+    private static int soilTemperature = 0;
+    private static int soilHumidity = 0;
+    private static int airTemperature = 0;
+    private static int airHumidity = 0;
 
     public TabFragment1() {
         // Required empty public constructor
@@ -98,9 +130,9 @@ public class TabFragment1 extends Fragment {
             public void run() {
                 while (flag) {
                     request();
+                    getRange();
                     try {
                         Thread.sleep(2000);
-                        Log.e(TAG, thread.getName()+"**********");
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -113,24 +145,56 @@ public class TabFragment1 extends Fragment {
     }
 
     private void request() {
-        Call<ZhuYeReception> call = BaseHttp.request().getSensor();
+        Call<ZhuYeReception> call = HttpUtils.request().getSensor();
         call.enqueue(new Callback<ZhuYeReception>() {
 
             @Override
             public void onResponse(Call<ZhuYeReception> call, Response<ZhuYeReception> response) {
                 Log.e(TAG, "request请求成功");
                 ZhuYeReception zhuYeReception = response.body();
-                tv_co2.setText(zhuYeReception.getCo2() + "");
-                tv_light.setText(zhuYeReception.getLight() + "");
-                tv_soilTemperature.setText(zhuYeReception.getSoilTemperature() + "");
-                tv_soilHumidity.setText(zhuYeReception.getSoilHumidity() + "");
-                tv_airTemperature.setText(zhuYeReception.getAirTemperature() + "");
-                tv_airHumidity.setText(zhuYeReception.getAirHumidity() + "");
+                tv_co2.setText((co2 = zhuYeReception.getCo2()) + "");
+                tv_light.setText((light = zhuYeReception.getLight()) + "");
+                tv_soilTemperature.setText((soilHumidity = zhuYeReception.getSoilTemperature()) + "");
+                tv_soilHumidity.setText((soilHumidity = zhuYeReception.getSoilHumidity()) + "");
+                tv_airTemperature.setText((airTemperature = zhuYeReception.getAirTemperature()) + "");
+                tv_airHumidity.setText((airHumidity = zhuYeReception.getAirHumidity()) + "");
             }
 
             @Override
             public void onFailure(Call<ZhuYeReception> call, Throwable throwable) {
-                Log.e(TAG, "请求失败");
+                Log.e(TAG, "getSensor请求失败");
+                Log.e(TAG, throwable.getMessage());
+            }
+        });
+    }
+
+    private void getRange() {
+        Call<GetConfig> getRange = HttpUtils.request().getConfig();
+        getRange.enqueue(new Callback<GetConfig>() {
+
+            @Override
+            public void onResponse(Call<GetConfig> call, Response<GetConfig> response) {
+                Log.e(TAG, "getRange请求成功");
+                GetConfig getConfig = response.body();
+                f1_minCo2.setText(getConfig.getMinCo2() + "");
+                f1_maxCo2.setText(getConfig.getMaxCo2() + "");
+                f1_minLight.setText(getConfig.getMinLight() + "");
+                f1_maxLight.setText(getConfig.getMaxLight() + "");
+                f1_minSoilTemperature.setText(getConfig.getMinSoilTemperature() + "");
+                f1_maxSoilTemperature.setText(getConfig.getMaxSoilTemperature() + "");
+                f1_minSoilHumidity.setText(getConfig.getMinSoilHumidity() + "");
+                f1_maxSoilHumidity.setText(getConfig.getMaxSoilHumidity() + "");
+                f1_minAirTemperature.setText(getConfig.getMinAirTemperature() + "");
+                f1_maxAirTemperature.setText(getConfig.getMaxAirTemperature() + "");
+                f1_minAirHumidity.setText(getConfig.getMinAirHumidity() + "");
+                f1_maxAirHumidity.setText(getConfig.getMaxAirHumidity() + "");
+
+                judgement(getConfig);
+            }
+
+            @Override
+            public void onFailure(Call<GetConfig> call, Throwable throwable) {
+                Log.e(TAG, "getRange请求失败");
                 Log.e(TAG, throwable.getMessage());
             }
         });
@@ -185,5 +249,39 @@ public class TabFragment1 extends Fragment {
         mUltraViewPager.setInfiniteLoop(true);
         //设定页面自动切换  间隔2秒
         mUltraViewPager.setAutoScroll(2000);
+    }
+
+    private void judgement(GetConfig getConfig) {
+        if (co2 > getConfig.getMinCo2() + 10 && co2 < getConfig.getMaxCo2() - 10)
+            img_p_co2.setImageResource(R.mipmap.p1);
+        else if (co2 > getConfig.getMinCo2() && co2 < getConfig.getMaxCo2())
+            img_p_co2.setImageResource(R.mipmap.p2);
+        else
+            img_p_co2.setImageResource(R.mipmap.p3);
+
+        if (light > getConfig.getMinLight() + 10 && light < getConfig.getMaxLight() - 10)
+            img_p_light.setImageResource(R.mipmap.p1);
+        else if (light > getConfig.getMinCo2() && light < getConfig.getMaxLight())
+            img_p_light.setImageResource(R.mipmap.p2);
+        else
+            img_p_light.setImageResource(R.mipmap.p3);
+
+        if (soilTemperature > getConfig.getMinSoilTemperature() + 100 && soilTemperature < getConfig.getMaxSoilTemperature() - 100 ||
+                soilHumidity > getConfig.getMinSoilHumidity() + 100 && soilHumidity < getConfig.getMaxSoilHumidity() - 100)
+            img_p_soil.setImageResource(R.mipmap.p1);
+        else if (soilTemperature > getConfig.getMinSoilTemperature() && soilTemperature < getConfig.getMaxSoilTemperature() ||
+                soilHumidity > getConfig.getMinSoilHumidity() && soilHumidity < getConfig.getMaxSoilHumidity())
+            img_p_soil.setImageResource(R.mipmap.p2);
+        else
+            img_p_soil.setImageResource(R.mipmap.p3);
+
+        if (airTemperature > getConfig.getMinAirTemperature() + 100 && airTemperature < getConfig.getMaxAirTemperature() - 100 ||
+                airHumidity > getConfig.getMinAirHumidity() + 100 && airHumidity < getConfig.getMaxAirHumidity() - 100)
+            img_p_air.setImageResource(R.mipmap.p1);
+        else if (airTemperature > getConfig.getMinAirTemperature() && airTemperature < getConfig.getMaxAirTemperature() ||
+                airHumidity > getConfig.getMinAirHumidity() && airHumidity < getConfig.getMaxAirHumidity())
+            img_p_air.setImageResource(R.mipmap.p2);
+        else
+            img_p_air.setImageResource(R.mipmap.p3);
     }
 }
